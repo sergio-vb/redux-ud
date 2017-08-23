403234bc040ec5ac7772d2806ff59ab9e359708f
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-08-23 15:10:41 -0600

    Adding initial files

---
+++ src/reducers/books.reducer.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: 'Javascript'},
+        { title: 'Harry Potter'},
+        { title: 'A Darker Shade of Magic'},
+        { title: 'The Final Empire'}
+    ]
+}
